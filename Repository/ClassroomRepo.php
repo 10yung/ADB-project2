49e255f616743fe2e35d14807121ddcfe3e44b33
LiuChiaHong <ga588950@gmail.com>
2017-05-03 20:49:24 +0800

    add Repository

---
+++ Repository/ClassroomRepo.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Repository;
+
+use Illuminate\Support\Facades\DB;
+
+
+class ClassroomRepo
+{
+    public static function addClassroom($name,$size){
+        DB::table('Classroom')
+            ->insert(
+                ['name' => $name, 'room_size' => $size]
+            );
+
+    }
+
+    public static function deleteClassroom($classroomID){
+        DB::table('Classroom')
+            ->where('roomID', '=', $classroomID)
+            ->delete();
+
+    }
+
+    public static function getClassroomByID($classroomID){
+        $classrom = DB::table('Classroom')
+            ->where('roomID', '=', $classroomID)
+            ->get();
+        
+        return $classrom;
+    }
+
+    public static function updateClassroomByID($classroomID, $name, $size){
+        $classrom = DB::table('Classroom')
+            ->where('roomID', '=', $classroomID)
+            ->update(['name' => $name, 'room_size' => $size]);
+
+    }
+}
---
+++ Repository/SysParamRepo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuchiahong
+ * Date: 2017/5/3
+ * Time: 下午8:31
+ */
+
+namespace App\Repository;
+
+use Illuminate\Support\Facades\DB;
+
+class SysParamRepo
+{
+
+}
