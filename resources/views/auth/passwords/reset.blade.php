938b1d6a3ddd0ace74505bdd10243ead2e99540d
Teng Yung Lin <andynumber10@gmail.com>
2017-05-10 20:24:39 +0800

    reset password style

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-primary" style="margin-top: 80px;">
                 <div class="panel-heading">Reset Password</div>
 
                 <div class="panel-body">
